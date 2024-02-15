73bd26b64e2f60cf3547261ba2bb24e69a0762f9
JoanaTabaku <joanatabaku15@gmail.com>
2024-02-15 16:58:30 +0100

    small fixes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,55 +11,70 @@
                         <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                     </div>
                     <form class="user" method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                    name="firstname" placeholder="First Name">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                    name="lastname" placeholder="Last Name">
-                            </div>
+                    @csrf
+                    <div class="form-group row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            <input type="text" class="form-control form-control-user" id="exampleFirstName"
+                                name="firstname" placeholder="First Name">
                         </div>
-                        <div class="form-group">
-                            <label for="role">Role</label>
-                            <select class="form-control" id="role" name="role">
-                                <option value="1">Perdorues</option>
-                                <option value="2">Pensionist</option>
-                                <option value="3">Invalid</option>
-                            </select>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control form-control-user" id="exampleLastName"
+                                name="lastname" placeholder="Last Name">
                         </div>
-                        <div class="form-group">
-                            <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                name="email" placeholder="Email Address">
+                    </div>
+                    <div class="form-group">
+                        <label for="role">Role</label>
+                        <select class="form-control" id="role" name="role">
+                            @foreach($roles as $role)
+                                <option value="{{ $role->id }}">{{ $role->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                            name="email" placeholder="Email Address">
+                        @error('email')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            <input type="password" class="form-control form-control-user" id="exampleInputPassword"
+                                name="password" placeholder="Password">
                         </div>
-                        <div class="form-group row">
-                            <div class="col-sm-6 mb-3 mb-sm-0">
-                                <input type="password" class="form-control form-control-user" id="exampleInputPassword"
-                                    name="password" placeholder="Password">
-                            </div>
-                            <div class="col-sm-6">
-                                <input type="password" class="form-control form-control-user" id="exampleRepeatPassword"
-                                    name="password_confirmation" placeholder="Repeat Password">
-                            </div>
+                        <div class="col-sm-6">
+                            <input type="password" class="form-control form-control-user" id="exampleRepeatPassword"
+                                name="password_confirmation" placeholder="Repeat Password">
                         </div>
-                        <button type="submit" class="btn btn-primary btn-user btn-block">Register</button>
-                        <hr>
-                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                            <i class="fab fa-google fa-fw"></i> Register with Google
-                        </a>
-                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                            <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                        </a>
-                    </form>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">Register</button>
+                    <hr>
+                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                        <i class="fab fa-google fa-fw"></i> Register with Google
+                    </a>
+                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                        <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                    </a>
+                </form>
+
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
 
                     <hr>
                     <div class="text-center">
                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                     </div>
                     <div class="text-center">
-                        <a class="small" href="login.html">Already have an account? Login!</a>
+                        <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
                     </div>
                 </div>
             </div>
